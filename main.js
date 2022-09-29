3c0a0a0494f3c190553613bb7b605d2e594366b0
wirovi07 <106172929+wirovi07@users.noreply.github.com>
2022-09-28 23:00:38 -0500

    calcular radio, perimetro

---
+++ main.js
@@ -0,0 +1,76 @@
+console.group('Cuadrado');
+const ladoCuadrado = 5;
+const perimetroCuadrado = ladoCuadrado * 4;
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+
+function calcularCuadrado(lado){
+    return{
+        perimetro: lado * 4,
+        area: lado * lado,
+    };
+}
+
+console.log({
+    ladoCuadrado,
+    perimetroCuadrado,
+    areaCuadrado,
+});
+
+console.groupEnd('Cuadrado');
+
+console.group('Triangulo');
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const ladoTrianguloBase = 4;
+const alturaTriangulo = 5.5;
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + ladoTrianguloBase;
+const areaTriangulo = (ladoTrianguloBase * alturaTriangulo ) /2 ;
+
+function calcularTriangulo(lado1, lado2, base, altura){
+    return{
+        perimetro: lado1 + lado2 + base,
+        area: (base * altura) / 2,
+    };
+}
+
+console.log({
+    ladoTriangulo1,
+    ladoTriangulo2,
+    ladoTrianguloBase,
+    alturaTriangulo,
+});
+
+console.groupEnd('Triangulo');
+
+console.group('Circle');
+
+const radioCirculo = 3;
+const diametroCirculo = radioCirculo * 2;
+const PI = 3.1415;
+
+const circunferencia = diametroCirculo * PI;
+const areaCirculo = (radioCirculo ** 2) * PI;
+
+console.log({
+    radioCirculo,
+    diametroCirculo,
+    PI,
+    circunferencia,
+    areaCirculo,
+});
+
+function calcularCirculo(radio){
+    
+    const diametro = radio * 2;
+    const radioAlCuadrado = Math.pow(radio, 2);
+    
+    return{
+        circunferencia: diametro * Math.PI,
+        area: radioAlCuadrado * Math.PI.toFixed(5),
+    }
+
+}
+
+console.groupEnd('Circle');
